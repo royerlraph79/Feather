72613efffb79432f9bb16d0f3e8d2ee382698c65
royerlraph79 <30907279+royerlraph79@users.noreply.github.com>
2026-04-16 14:05:22 -0400

    Update SigningView.swift

--- Feather/Views/Signing/SigningView.swift
+++ Feather/Views/Signing/SigningView.swift
@@ -225,13 +225,13 @@ extension SigningView {
 						options: $_temporaryOptions.optional()
 					)
 				}
-				#if NIGHTLY || DEBUG
-					NavigationLink(.localized("Entitlements") + " (BETA)") {
+
+					NavigationLink(.localized("Entitlements")) {
 						SigningEntitlementsView(
 							bindingValue: $_temporaryOptions.appEntitlementsFile
 						)
 					}
-				#endif
+
 				NavigationLink(.localized("Tweaks")) {
 					SigningTweaksView(
 						options: $_temporaryOptions
